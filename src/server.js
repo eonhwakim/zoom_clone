a0db1b91225e4d9ca936a9d96d8d0c32b2f702aa
eonhwa kim <eonhwa97@gmail.com>
2025-01-01 16:38:16 +0900

    websocket init

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -9,4 +11,9 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const handelListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handelListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handelListen);
